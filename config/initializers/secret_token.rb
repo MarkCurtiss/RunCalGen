6429fa212bba4867b0df8efe01a2e40d816a37d2
Mark Curtiss <MarkCurtiss@gmail.com>
2010-11-07 15:39:03 -0800

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RunCalGen::Application.config.secret_token = '12f34a232dfe7ec10afdc52dea2caf6a6fd27f313feb4707b088d81ff390e88eb19f0b2df826e01a9c9f835866404c10e1d9914b25423cc7235ff959d2671993'
