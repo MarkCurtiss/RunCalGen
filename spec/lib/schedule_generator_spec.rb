e143a6ad056b2fa173b560bd26b5160f38cffc8b
Mark Curtiss <MarkCurtiss@gmail.com>
2010-12-17 18:10:18 -0800

    added race distance constants

--- spec/lib/schedule_generator_spec.rb
+++ spec/lib/schedule_generator_spec.rb
@@ -2,8 +2,28 @@ require 'spec_helper'
 
 describe ScheduleGenerator do
   describe '#calculate_pace' do
-    it 'should determine pace given a race distance and a time' do
-      ScheduleGenerator.calculate_pace('10K', '00:53:56').should == '8:40'
+    it 'should determine pace for a 5k race' do 
+      ScheduleGenerator.calculate_pace(Race::FIVE_K, '00:25:56').should == '8:20'
+    end
+
+    it 'should determine pace for a 5M race' do
+      ScheduleGenerator.calculate_pace(5, '00:42:38').should == '8:31'
+    end
+
+    it 'should determine pace for a 10k race' do
+      ScheduleGenerator.calculate_pace(Race::TEN_K, '00:53:56').should == '8:40'
+    end
+
+    it 'should determine pace for a 10M race' do
+      ScheduleGenerator.calculate_pace(10, '1:29:20').should == '8:56'
+    end
+
+    it 'should determine pace for a half marathon' do
+      ScheduleGenerator.calculate_pace(Race::HALF_MARATHON, '1:58:06').should == '9:00'
+    end
+
+    it 'should determine pace for a marathon' do
+      ScheduleGenerator.calculate_pace(Race::MARATHON, '4:13:40').should == '9:40'
     end
   end
 end
